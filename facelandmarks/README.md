80656f27f0c413803d918c47baf03a1cbfdfc405
Arsen Shkrumelyak <arsen@shkrumelyak.com>
2024-09-23 10:14:47 +0100

    facelandmarks

---
+++ facelandmarks/README.md
@@ -0,0 +1,113 @@
+# Face Landmark Detection
+
+This API is based on the work found
+[here](https://github.com/midasklr/facelandmarks).
+
+It’s designed to be used with Request Directory and you can find more details
+[here](https://request.directory/facelandmarks)
+
+## Development
+
+### Install dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run the API
+
+```bash
+python src/main.py
+```
+
+## Usage
+
+By default, the API runs on port 7000.
+
+```bash
+docker run -it -p7000:7000 arsenstorm/facelandmarks:latest
+```
+
+## API
+
+To use the API, you need to send a POST request containing form data to the
+`/landmarks` endpoint with the following parameters:
+
+#### Parameters
+
+- `image`: The image to detect landmarks on.
+
+#### Example Request
+
+As an example, we’ll upload this image:
+
+<img src="../.github/facelandmarks/example_input.jpg" alt="example_input" style="max-width: 500px;">
+
+```bash
+curl -X POST http://localhost:7000/landmarks -F "image=@.github/facelandmarks/example_input.jpg"
+```
+
+#### Example Response
+
+We get the following response:
+
+```json
+{
+  "bounds": {
+    "x1": 339,
+    "x2": 670,
+    "y1": 188,
+    "y2": 605
+  },
+  "confidence": 0.9646592140197754,
+  "face": {
+    "left_eye": {
+      "x": 425,
+      "y": 375
+    },
+    "left_mouth": {
+      "x": 476,
+      "y": 494
+    },
+    "nose": {
+      "x": 518,
+      "y": 412
+    },
+    "right_eye": {
+      "x": 560,
+      "y": 315
+    },
+    "right_mouth": {
+      "x": 615,
+      "y": 438
+    }
+  },
+  "image": "/9j/4AAQSkZJRgA...", // shortened for brevity
+  "landmarks": [
+    {
+      "x": 362,
+      "y": 395
+    },
+    // ...
+    {
+      "x": 550,
+      "y": 324
+    }
+  ],
+  "success": true
+}
+```
+
+A total of 98 landmarks are returned, alongside the face bounds and the
+confidence of the detection.
+
+The image is returned as a base64 encoded string, which can be decoded where it’ll
+look like this:
+
+<img src="../.github/facelandmarks/example_output.jpg" alt="example_output" style="max-width: 500px;">
+
+> [!TIP]
+>
+> You can test out the API using
+> [Request Directory](https://request.directory/facelandmarks) without needing
+> to run it locally.
---
+++ facelandmarks/src/main.py
@@ -0,0 +1,146 @@
+import io
+import os
+import cv2
+import torch
+import base64
+import numpy as np
+from PIL import Image
+from dotenv import load_dotenv
+from torchvision import transforms
+from faceland import FaceLanndInference
+from hdface.hdface import hdface_detector
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+DEBUG_MODE = os.getenv('FACELANDMARKS_DEBUG', 'false').lower() == 'true'
+
+app = Flask(__name__)
+
+
+def detect_landmarks(image):
+    """This function detects face landmarks on an image."""
+    det = hdface_detector(use_cuda=False)
+    checkpoint = torch.load(
+        './facelandmarks/src/faceland.pth', weights_only=True)
+    plfd_backbone = FaceLanndInference()
+    plfd_backbone.load_state_dict(checkpoint)
+    plfd_backbone.eval()
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    image = Image.open(io.BytesIO(image.read()))
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+
+    if image is not None:
+        height, width = image.shape[:2]
+        img_det = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        result = det.detect_face(img_det)
+        for i in range(len(result)):
+            box = result[i]['box']
+            cls = result[i]['cls']
+            pts = result[i]['pts']
+
+            points = {
+                "left_eye": {"x": pts["leye"][0], "y": pts["leye"][1]},
+                "left_mouth": {"x": pts["lmouse"][0], "y": pts["lmouse"][1]},
+                "nose": {"x": pts["nose"][0], "y": pts["nose"][1]},
+                "right_eye": {"x": pts["reye"][0], "y": pts["reye"][1]},
+                "right_mouth": {"x": pts["rmouse"][0], "y": pts["rmouse"][1]}
+            }
+
+            bounds = {
+                "x1": box[0],
+                "y1": box[1],
+                "x2": box[2],
+                "y2": box[3]
+            }
+
+            try:
+                confidence = cls.tolist()[0] or "unknown"
+            except (IndexError, AttributeError):
+                confidence = "unknown"
+
+            # Calculate the box dimensions and center as before
+            x1, y1, x2, y2 = box
+
+            w = x2 - x1 + 1
+            h = y2 - y1 + 1
+
+            size_w = int(max([w, h]) * 0.8)
+            size_h = int(max([w, h]) * 0.8)
+            cx = x1 + w // 2
+            cy = y1 + h // 2
+            x1 = cx - size_w // 2
+            x2 = x1 + size_w
+            y1 = cy - int(size_h * 0.4)
+            y2 = y1 + size_h
+
+            # Clip the coordinates to ensure they stay within the image boundaries
+            x1 = max(0, int(x1))
+            y1 = max(0, int(y1))
+            x2 = min(width, int(x2))
+            y2 = min(height, int(y2))
+
+            # Create the bounding box on the original image
+            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0))
+
+            # Prepare the cropped version for landmark detection, but the landmarks will be mapped back to the full image
+            cropped = image[y1:y2, x1:x2]
+            cropped = cv2.resize(cropped, (112, 112))
+
+            image_input = cv2.resize(cropped, (112, 112))
+            image_input = cv2.cvtColor(image_input, cv2.COLOR_BGR2RGB)
+            image_input = transform(image_input).unsqueeze(0)
+            landmarks = plfd_backbone(image_input)
+
+            # Get the landmark positions in the cropped space
+            pre_landmark = landmarks[0]
+            pre_landmark = pre_landmark.cpu().detach(
+            ).numpy().reshape(-1, 2) * [size_w, size_h]
+
+            # Map the landmarks back to the original image coordinates
+            for (x, y) in pre_landmark.astype(np.int32):
+                absolute_x = x1 + x  # Adjust x relative to the whole image
+                absolute_y = y1 + y  # Adjust y relative to the whole image
+                cv2.circle(image, (absolute_x, absolute_y),
+                           2, (255, 0, 255), 2)
+
+            # Return the original image landmarks relative to the whole image
+            landmarks_full_image = [
+                {"x": int(x1 + x), "y": int(y1 + y)} for (x, y) in pre_landmark]
+
+            # Return the image with landmarks in base64
+            _, buffer = cv2.imencode('.jpg', image)
+            image_base64 = base64.b64encode(buffer).decode('utf-8')
+
+            return jsonify({
+                "bounds": bounds,
+                "confidence": confidence,
+                "face": points,
+                "landmarks": landmarks_full_image,
+                "image": image_base64,
+                "success": True
+            }), 200
+    else:
+        return jsonify({"error": "The file you have uploaded is invalid.", "success": False}), 400
+
+
+@app.route('/landmarks', methods=['POST'])
+def landmarks():
+    if 'image' not in request.files:
+        return jsonify({"error": "You haven’t included an image in the `image` parameter.", "success": False}), 400
+
+    image = request.files['image']
+
+    if image.filename == '':
+        return jsonify({"error": "The file you have uploaded is invalid.", "success": False}), 400
+
+    try:
+        return detect_landmarks(image)
+    except Exception as e:
+        return jsonify({"error": str(e), "success": False}), 500
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('FACELANDMARKS_PORT', 7000))
+    app.run(host='0.0.0.0', port=port, debug=DEBUG_MODE)
